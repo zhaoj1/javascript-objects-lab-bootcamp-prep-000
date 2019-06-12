991220cd7e68eef3ae80299c7614b99cd12fa725
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-12 21:03:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object,key){
-  return Object.assign({}, object, { [key]: value});
+  
   
 }
