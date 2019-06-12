042bac189fbb67c6218a24c305c7c580934784d1
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-12 21:00:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({}, object, { [key]: value });
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key]
 }
